d6cfbc7f996cf0895202085e706a1924f729c869
dbs07x <dbs07x@gmail.com>
2022-05-20 16:05:16 -0300

    20220418

---
+++ Notebook/CSharp20220418.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    internal class CSharp20220418
+    {
+        public static void Main(params string[] args)
+        {
+            Console.Write("Olá Mundo!");
+            Console.Write(""); //Escreve na mesma linha
+            Console.WriteLine(""); //Escreve na próxima linha
+            Console.WriteLine("Soma: 2 + 2"); //Soma: 2 + 2
+            Console.WriteLine("Soma: " + (2 + 2)); //Soma: 4
+            Console.WriteLine("Soma: " + 2 + 2); //Soma: 22
+            //string = cadeia de caracteres, palavras, frases
+            //"string" + "string" = concatenação de strings
+        }
+    }
+}
