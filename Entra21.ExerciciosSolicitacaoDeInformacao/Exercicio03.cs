ea5f540625a832403e53357100cecf58c35868e8
dbs07x <dbs07x@gmail.com>
2022-05-24 13:53:38 -0300

    Entra21.ExerciciosSolicitacaoDeInformacao.Exercicio03

---
+++ Entra21.ExerciciosSolicitacaoDeInformacao/Exercicio03.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosSolicitacaoDeInformacao
+{
+    internal class Exercicio03
+    {
+        public void Executar()
+        {
+            // Solicite a altura e peso e apresente o IMC desta pessoa. Fórmula IMC: peso / altura²
+
+            Console.WriteLine("Informe seu Peso (kg): ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe sua Altura(m): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            Console.Clear();
+            Console.WriteLine("Seu IMC é de: " + imc);
+        }
+    }
+}
