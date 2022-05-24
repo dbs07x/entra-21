1e017bcf0dd382d7bd15c10cd79261cf9896933b
dbs07x <dbs07x@gmail.com>
2022-05-24 14:02:09 -0300

    Entra21.ExerciciosSolicitacaoDeInformacao.Exercicio05

---
+++ Entra21.ExerciciosSolicitacaoDeInformacao/Exercicio05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosSolicitacaoDeInformacao
+{
+    internal class Exercicio05
+    {
+        public void Executar()
+        {
+            //Solicite um número para o usuário e apresente ao final seu antecessor e o sucessor
+
+            Console.WriteLine("Informe um número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("O número antecessor do número informado é: " + (numero - 1));
+            Console.WriteLine("O número sucessor do número informado é: " + (numero + 1));
+        }
+    }
+}
