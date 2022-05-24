36ad23cf7364635d92d0e69835f23877b7b9eec9
dbs07x <dbs07x@gmail.com>
2022-05-24 13:44:44 -0300

    Entra21.ExerciciosSolicitacaoDeInformacao.Exercicio02

---
+++ Entra21.ExerciciosSolicitacaoDeInformacao/Exercicio02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosSolicitacaoDeInformacao
+{
+    internal class Exercicio02
+    {
+        public void Executar()
+        {
+            //Solicite o nome e sobrenome e apresente o nome completo
+
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Nome completo: " + nome + " " + sobrenome);
+        }
+    }
+}
--- Entra21.ExerciciosSolicitacaoDeInformacao/Program.cs
+++ Entra21.ExerciciosSolicitacaoDeInformacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Entra21.ExerciciosSolicitacaoDeInformacao;
 
 Console.WriteLine(@"1 - Exercício 01
+2 - Exercício 02
 ");
 
 Console.WriteLine("Escolha um item no menu: ");
@@ -10,4 +11,9 @@ if (menu == 1)
 {
     Exercicio01 exercicio01 = new Exercicio01();
     exercicio01.Executar();
+}
+if (menu == 2)
+{
+    Exercicio02 exercicio02 = new Exercicio02();
+    exercicio02.Executar();
 }
