956b464c441368a3bec79e7ec74e37e3ef799af2
dbs07x <dbs07x@gmail.com>
2022-05-25 15:24:19 -0300

    Entra21.ExerciciosWhile.Exercicio01

---
+++ Entra21.ExerciciosWhile/Exercicio01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            //Solicite o preço e nome da peça de 13 peças
+
+            int indice = 0;
+            string nome = "";
+            double preco = 0;
+
+            while (indice < 13)
+            {
+                Console.WriteLine("Nome da peça: ");
+                nome = Console.ReadLine().Trim();
+
+                Console.WriteLine("Preço da peça: ");
+                preco = Convert.ToDouble(Console.ReadLine());
+
+                indice = indice + 1;
+            }
+        }
+    }
+}
---
+++ Entra21.ExerciciosWhile/Program.cs
@@ -0,0 +1,13 @@
+﻿using Entra21.ExerciciosWhile;
+
+Console.WriteLine(@"1 - Exercício 01
+");
+
+Console.WriteLine("Escolha um item no menu: ");
+var menu = Convert.ToInt32(Console.ReadLine());
+
+if (menu == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
