77f214e774b7e1574311c8039aeb5a68106b7d8f
dbs07x <dbs07x@gmail.com>
2022-05-25 15:33:18 -0300

    Entra21.ExerciciosWhile.Exercicio03

---
+++ Entra21.ExerciciosWhile/Exercicio03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio03
+    {
+        public void Executar()
+        {
+            // Solicite a idade para o usuário até que ele digite uma idade maior que 128
+
+            int idade = 0;
+
+            while (idade <= 128)
+            {
+                Console.WriteLine("Idade: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
