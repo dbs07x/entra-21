2710d372a696a37a9a723e417ee517712d38ef53
dbs07x <dbs07x@gmail.com>
2022-05-25 15:30:25 -0300

    Entra21.ExerciciosWhile.Exercicio02

---
+++ Entra21.ExerciciosWhile/Exercicio02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio02
+    {
+        public void Executar()
+        {
+            //Solicite nomes ao usuário até que o mesmo digite fim para o nome
+
+            string nome = "";
+
+            while (nome != "fim")
+            {
+                Console.WriteLine("Digite o nome: ");
+                nome = Console.ReadLine().ToLower().Trim();
+            }
+        }
+    }
+}
