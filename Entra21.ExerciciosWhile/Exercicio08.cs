cf4c8c0eeb577ff25efb4ab74cc0fb3263198b53
dbs07x <dbs07x@gmail.com>
2022-05-26 15:57:18 -0300

    Entra21.ExerciciosWhile.Exercicio08

---
+++ Entra21.ExerciciosWhile/Exercicio08.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio08
+    {
+        public void Executar()
+        {
+            //Solicite um número e apresentar a tabuada do mesmo até 1000
+
+            Console.WriteLine("Informe um número: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+
+            int indice = 0;
+
+            Console.Clear();
+            while (indice < 1000)
+            {
+                indice = indice + 1;
+                double resultado = numero * indice;
+                Console.WriteLine(numero + " x " + indice + " = " + resultado);
+            }
+        }
+    }
+}
