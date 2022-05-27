5b8de9e3fdbb5ee7b17715a022928c5db72bd288
dbs07x <dbs07x@gmail.com>
2022-05-27 16:01:33 -0300

    Entra21.ExerciciosWhile.Exercicio09

---
+++ Entra21.ExerciciosWhile/Exercicio09.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio09
+    {
+        public void Executar()
+        {
+            //Solicite um número e faça o fatorial deste número, utilizando while
+
+            Console.WriteLine("Número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int indice = numero;
+            int numeroSolicitado = numero;
+
+            while (indice > 1)
+            {
+                indice = indice - 1;
+                numero = numero * indice;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Fatorial de " + numeroSolicitado + ":" + numero);
+        }
+    }
+}
