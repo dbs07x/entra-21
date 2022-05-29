ff2ba323efa7788aa779f1c72b17fea3f10c1f18
dbs07x <dbs07x@gmail.com>
2022-05-29 20:00:14 -0300

    Entra21.ExemplosOrientacaoObjetos.Pessoas.Pessoa

---
+++ Entra21.ExemplosOrientacaoObjetos/Pessoas/ExemploPessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Pessoas
+{
+    public class ExemploPessoa
+    {
+        public void Executar()
+        {
+            var claudinho = new Pessoa();
+            claudinho.Nome = "Claudinho";
+            claudinho.Altura = 2.03;
+            claudinho.Peso = 98;
+
+            claudinho.CalcularImc();
+            claudinho.ApresentarImc();
+        }
+    }
+}
---
+++ Entra21.ExemplosOrientacaoObjetos/Pessoas/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Pessoas
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public double Altura;
+        public double Peso;
+
+        public double CalcularImc()
+        {
+            var imc = Peso / (Altura * Altura);
+            Console.WriteLine("IMC: " + imc);
+
+            return imc;
+        }
+
+        public void ApresentarImc()
+        {
+            var imcAuxiliar = CalcularImc();
+
+            if (imcAuxiliar < 18.5)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imcAuxiliar < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade");
+            }
+        }
+    }
+}
