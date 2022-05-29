7604023ab69fe32fba99c1bd37859074da4ec4e9
dbs07x <dbs07x@gmail.com>
2022-05-29 19:55:40 -0300

    Entra21.ExemplosOrientacaoObjetos.Calculadoras.Calculadora

---
+++ Entra21.ExemplosOrientacaoObjetos/Calculadoras/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Calculadoras
+{
+    public class Calculadora
+    {
+        // Propriedades
+        public int Numero1;
+        public int Numero2;
+
+        // tipoEncapsulamento + tipoRetorno + NomeMetodo(parametros)
+        public void Somar()
+        {
+            var soma = Numero1 + Numero2;
+            Console.WriteLine($"Soma: {soma}");
+        }
+        public void Subtrair()
+        {
+            var subtracao = Numero1 - Numero2;
+            Console.WriteLine($"Subtração: {subtracao}");
+        }
+    }
+}
---
+++ Entra21.ExemplosOrientacaoObjetos/Calculadoras/ExemploCalculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Calculadoras
+{
+    public class ExemploCalculadora
+    {
+        public void Executar()
+        {
+            Calculadora hp = new Calculadora();
+            Console.Write("Digite o número 1:");
+            hp.Numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número 2:");
+            hp.Numero2 = Convert.ToInt32(Console.ReadLine());
+
+            hp.Somar();
+            hp.Subtrair();
+
+            Calculadora epson = new Calculadora();
+            Console.Write("Digite o número 1:");
+            epson.Numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número 2:");
+            epson.Numero2 = Convert.ToInt32(Console.ReadLine());
+
+            epson.Somar();
+            epson.Subtrair();
+        }
+    }
+}
