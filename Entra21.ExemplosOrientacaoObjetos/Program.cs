fd232b36866be02e29582ae26d2dccca8c975e11
dbs07x <dbs07x@gmail.com>
2022-05-29 20:01:20 -0300

    Entra21.ExemplosOrientacaoObjetos.Produtos.Produto

--- Entra21.ExemplosOrientacaoObjetos/Program.cs
+++ Entra21.ExemplosOrientacaoObjetos/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entra21.ExemplosOrientacaoObjetos.Calculadoras;
+using Entra21.ExemplosOrientacaoObjetos.Carros;
+using Entra21.ExemplosOrientacaoObjetos.Contas;
+using Entra21.ExemplosOrientacaoObjetos.Pessoas;
+
+Console.WriteLine(@"1 - Calculadora
+2 - Carros
+3 - Contas
+4 - Pessoas
+");
+
+Console.WriteLine("Escolha um item no menu: ");
+var menu = Convert.ToInt32(Console.ReadLine());
+
+if (menu == 1)
+{
+    ExemploCalculadora exemploCalculadora = new ExemploCalculadora();
+    exemploCalculadora.Executar();
+}
+if(menu == 2)
+{
+    ExemploCarro exemploCarro = new ExemploCarro();
+    exemploCarro.Executar();
+}
+if (menu == 3)
+{
+    ExemploConta exemploConta = new ExemploConta();
+    exemploConta.Executar();
+}
+if (menu == 4)
+{
+    ExemploPessoa exemploPessoa = new ExemploPessoa();
+    exemploPessoa.Executar();
+}
