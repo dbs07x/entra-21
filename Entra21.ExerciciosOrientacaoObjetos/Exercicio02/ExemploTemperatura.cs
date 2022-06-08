7504d9906f9f6bc5e440851f563001392ee7e574
dbs07x <dbs07x@gmail.com>
2022-06-07 23:56:04 -0300

    Entra21.ExerciciosOrientacaoObjetos.Exercicio02.ExemploTemperatura

---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio02/ExemploTemperatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio02
+{
+    public class ExemploTemperatura
+    {
+        public void Executar()
+        {
+            Temperatura temperatura = new Temperatura();
+            temperatura.TemperaturaOrigem = "celsius";
+            temperatura.TemperaturaDestino = "kelvin";
+            temperatura.TemperaturaValor = 30;
+
+            Console.WriteLine("Temperatura convertida: " + temperatura.ObterTemperaturaConvertida());
+        }
+    }
+}
