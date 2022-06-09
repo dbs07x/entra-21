ff54f4225148662a26c24c8d3547a8451b9af24d
dbs07x <dbs07x@gmail.com>
2022-06-09 20:32:54 -0300

    Entra21.ExerciciosLista.ExercicioProduto

--- Entra21.EexerciciosLista/ExercicioProduto/ProdutoControlador.cs
+++ Entra21.EexerciciosLista/ExercicioProduto/ProdutoControlador.cs
@@ -6,12 +6,71 @@
 
         public void GerenciarMenu()
         {
-            Cadastrar();
-            ApresentarProdutos();
-            Cadastrar();
-            ApresentarProdutos();
+            int codigo = 0;
+            //Repete enquanto o código não for o menu sair(6)
+            while (codigo != 6)
+            {
+                Console.Clear();
+                //Apresentar o menu e solicita o código
+                codigo = ApresentarSolicitarMenu();
+
+                if (codigo == 1)
+                {
+                    // Menu escolhido para listas produtos
+                    ApresentarProdutos();
+                }
+                else if (codigo == 2)
+                {
+                    // Menu escolhido para cadastrar produto
+                    Cadastrar();
+                }
+                else if (codigo == 3)
+                {
+                    // Mene escolhido para editar produto
+                    // Editar();
+                }
+                else if (codigo == 4)
+                {
+                    // Apagar();
+                }
+                else if (codigo == 5)
+                {
+                    //ApresentarProduto();
+                }
+                Thread.Sleep(1000);
+            }
         }
 
+        private int ApresentarSolicitarMenu()
+        {
+            Console.WriteLine(@" MENU:
+1 - Listar todos
+2 - Cadastrar
+3 - Editar
+4 - Apagar
+5 - Apresentar produto desejado
+6 - Sair");
+            int codigo = SolicitarCodigo();
+            return codigo;
+        }
+        private int SolicitarCodigo()
+        {
+            int codigo = 0;
+            // Continua solicitando o código até que seja um código entre 1 e 6
+            while (codigo < 1 || codigo > 6)
+            {
+                try
+                {
+                    Console.Write("Digite o código: ");
+                    codigo = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Digite um menu válido");
+                }
+            }
+            return codigo;
+        }
         private void Cadastrar()
         {
             Console.WriteLine("Nome: ");
@@ -23,13 +82,16 @@
             Console.Write("Preço unitário: ");
             var precoUnitario = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(@"Localizações disponíveis: 
+- Armazem
+- Area Venda
+- Loja");
             Console.Write("Localização: ");
             var localizacao = Console.ReadLine();
 
             var localizacaoProdutos = ObterLocalizacaoProduto(localizacao);
 
             produtoServico.Adicionar(nome, precoUnitario, localizacaoProdutos, quantidade);
-
         }
         private ProdutoLocalizacao ObterLocalizacaoProduto(string localizacao)
         {
@@ -51,7 +113,14 @@
         {
             var produtos = produtoServico.ObterTodos();
 
-            for(var i = 0; i < produtos.Count; i++)
+            if(produtos.Count() == 0)
+            {
+                Console.WriteLine("Nenhum produto cadastrado: ");
+                // Para a execução deste método
+                return;
+            }
+
+            for (var i = 0; i < produtos.Count; i++)
             {
                 var produtoAtual = produtos[i];
 
--- Entra21.EexerciciosLista/ExercicioProduto/ProdutoServico.cs
+++ Entra21.EexerciciosLista/ExercicioProduto/ProdutoServico.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entra21.ExerciciosLista
+﻿namespace Entra21.ExerciciosLista
 {
     internal class ProdutoServico
     {
+        // encapsulamento + tipoRetorno + NomeMetodo(parametros) -> assinatura
+        // o que fica entre as {} é o corpo do método
         private List<Produto> produtos = new List<Produto>();
-        // encapsulamento + tipoRetorno + NomeMetodo(parametros)
+        // Armazenar o código do próximo produto
+        private int codigoAtual = 1;
 
         public void Adicionar(string nome, double precoUnitario, ProdutoLocalizacao localizacao, int quantidade)
         {
@@ -22,18 +19,88 @@ namespace Entra21.ExerciciosLista
             produto.Localizacao = localizacao;
             produto.Quantidade = quantidade;
 
+            // Atribuir o código novo para o objeto do produto
+            produto.Codigo = codigoAtual;
+
+            // Adicionar 1 no código atual para quando criar novo produto o produto conter outro código
+            codigoAtual = codigoAtual + 1;
+
             // Adicionar o produto instanciado na lista de produtos
             produtos.Add(produto);
         }
-        public void Editar() { }
-        public void Apagar() { }
+        public bool Editar(
+        int codigoParaAlterar,
+        string nome,
+        double precoUnitario,
+        ProdutoLocalizacao localizacao,
+        int quantidade)
+        {
+            // Obter o produto desejado da lista de produto
+            Produto produtoParaAlterar = ObterPorCodigo(codigoParaAlterar);
+
+            // Verificar se não foi possível encontrar o produto
+            if (produtoParaAlterar == null)
+            {
+                // Retorno falso pq não existe produto com o código dos parâmetros
+                return false;
+            }
+
+            // Atualiza as propriedades (campos) do produto desejado
+            produtoParaAlterar.Nome = nome;
+            produtoParaAlterar.Localizacao = localizacao;
+            produtoParaAlterar.PrecoUnitario = precoUnitario;
+            produtoParaAlterar.Quantidade = quantidade;
+
+            //Retorna verdadeiro pq foi possível alterar o produto
+            return true;
+
+        }
+        public bool Apagar(int codigo)
+        {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Resgatando de uma lista de tipos primitivos
+                // int numero  = numeros[i]
+
+                // Resgatando de uma lista de objetos
+                Produto produto = produtos[i];
+                if (produto.Codigo == codigo)
+                {
+                    // Posso remover com o indice do objeto desejado
+                    // produtos.RemoveAt(i);
+
+                    // Posso remover com o objeto desejado
+                    produtos.Remove(produto);
+                    // Retorna verdadeiro pq o produto com o código desejado foi removido da lista
+                    return true;
+                }
+            }
+            // Retorna falso pq não foi encontrado produto com o código desejado, ou seja, nenhum elemento foi removido da lista
+            return false;
+        }
         public List<Produto> ObterTodos()
         {
             return produtos;
         }
 
-        public Produto ObterPorCodigo()
+        public Produto ObterPorCodigo(int codigo)
         {
+            // Percorrer todos os elementos para encontrar o produto
+            // Filtrando por código
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Obter o produto da lista de produtos em determinado indice
+                var produtoAtual = produtos[i];
+
+                // Verifica se o produto atual da lista percorrida é o desejado
+                if (produtoAtual.Codigo == codigo)
+                {
+                    // Retorno o produto encontrado
+                    return produtoAtual;
+                }
+            }
+
+            // Retorna null, caso não encontre um produto com o código passado como parâmetro
             return null;
         }
     }
