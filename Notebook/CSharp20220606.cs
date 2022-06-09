89692bc41ce7c6018b936582e73678020e183390
dbs07x <dbs07x@gmail.com>
2022-06-09 18:21:50 -0300

    20220606

---
+++ Notebook/CSharp20220606.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    internal class CSharp20220606
+    {
+        /*int[] numeros = new int [4]
+        numeros [0] = 28;
+        numeros [1] = 9;
+        numeros [2] = 18;
+        numeros [3] = 15;
+        int quantidadeElementos = numeros.Lenght;*/
+
+        //CRUD = Create, Read, Update, Delete
+
+        /*List<int> numeros = new List<int>();
+        numeros.Add(28);
+        numeros.Add(9);
+        numeros.Add(18);
+        numeros.Add(15);
+        int quantidadeElementos = numeros.Count;
+        // índice específico
+        numeros.Insert(2,50);
+        // Alterar
+        numeros[3] = 32;
+        // Remover elemento
+        numeros.Remove(9);
+        // Remover elemento por índice
+        numeros.RemoveAt(2);
+        // Descobrir índice
+        var indiceNumero50 = numeros.IndexOf(50);
+        // Existe na lista
+        var contemNumero9 = numeros.Contains(9);
+        // Apresentar
+        CW("[0]:" + numeros [0] +
+        "\n[1]:" + numeros [1] +
+        "\n[2]:" + numeros [2]);
+        //Conversão de votos para lista
+        List <int> listaNumeros = numeros.ToList();
+        //Conversão de lista para vetor
+        int[] numerosVetor = listaNumeros.ToArrange();
+        //Como alterar registro buscando por elemento
+        var indiceNome = nomes.IndexOf("Amanda");
+        nomes [indiceNome] = Console.ReadLine();*/
+    }
+}
