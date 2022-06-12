bea4ce045ae220f9112103aae1c5e8ceac2cf1fa
dbs07x <dbs07x@gmail.com>
2022-06-11 21:47:57 -0300

    Entra21.ExerciciosListaObjetos.Exercicio02

---
+++ Entra21.ExerciciosListaObjetos/Exercicio02/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace Entra21.ExerciciosListaObjetos.Exercicio02
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public int CodigoMatricula;
+        public int Idade;
+        public string MateriaFavorita;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+    }
+}
