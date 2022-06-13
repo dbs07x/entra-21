05ee238caf28a15eeb9abe282ed3e69b10d64df4
dbs07x <dbs07x@gmail.com>
2022-06-12 22:45:47 -0300

    Entra21.ExerciciosListaObjetos.Exercicio02

---
+++ Entra21.ExerciciosListaObjetos/Exercicio02/AlunoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosListaObjetos.Exercicio02
+{
+    internal enum AlunoStatus
+    {
+        Aprovado,
+        Reprovado,
+        EmExame
+    }
+}
