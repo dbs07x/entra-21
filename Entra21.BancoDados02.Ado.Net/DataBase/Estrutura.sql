8e156bcb404f224cc87ff2c12469dc98e921fc91
dbs07x <dbs07x@gmail.com>
2022-07-21 23:45:53 -0300

    Entra21.BancoDados02.Ado.Net

---
+++ Entra21.BancoDados02.Ado.Net/DataBase/Estrutura.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE unidades_federativas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+
+	nome VARCHAR(100),
+	sigla VARCHAR(2));
+
+CREATE TABLE cidades(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+
+	id_unidade_federativa INTEGER,
+
+	nome VARCHAR(100),
+	quantidade_habitantes INTEGER,
+	data_hora_fundacao DATETIME2,
+	pib DECIMAL(16,2)
+
+	FOREIGN KEY(id_unidade_federativa) REFERENCES unidades_federativas(id));
+
+SELECT * FROM cidades, unidades_federativas
